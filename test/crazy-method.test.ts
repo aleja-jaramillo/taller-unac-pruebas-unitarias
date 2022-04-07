7d1f73823ef30cf5ea561d41f84783143f65d189
Alejandra Jaramillo <yitneda.jaramillov@unac.edu.co>
2022-04-06 20:59:41 -0500

    agregue 7 test nuevos

---
+++ test/crazy-method.test.ts
@@ -0,0 +1,13 @@
+import { crazy } from '../src/crazy-method';
+//Test bueno
+test("Test resultado = 0 + number1 = 2 ", () =>{
+    //probar
+    expect(crazy(2,3)).toBe(8);
+});
+//Test malo
+test("Test resultado = 0 + number1 = 2 ", () =>{
+    //probar
+    expect(crazy(2,3)).toBe(5);
+});
+
+
